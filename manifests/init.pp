98501f8c78f248f3d9381c722337f4ba132d59f9
Hao SUN <xsunsmile@gmail.com>
2011-05-25 00:07:58 +0900

    tiny fix

--- manifests/init.pp
+++ manifests/init.pp
@@ -120,7 +120,7 @@ class mongodb {
 
 	define mongofile_put {
 		exec { "mongofile_put_${name}":
-			command => "mongofiles -r --host ${MONGO_HOST} put ${name}",
+			command => "mongofiles -r --host ${mongodb_host} put ${name}",
 			require => Service["mongodb"],
 		}
 	}
